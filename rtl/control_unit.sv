5c307ba4efe306ebf3831256808c5408ab6211b6
VEEETOOORRR <antonni2601.ag@gmail.com>
2026-07-10 03:13:31 -0300

    muitas correções aqui

--- rtl/control_unit.sv
+++ rtl/control_unit.sv
@@ -5,27 +5,21 @@ module control_unit (
     input logic cancel,
 
     input logic [1:0] coin_in,
-    input logic [1:0] sel_item,
 
     output logic dispense,
-    output logic change_out,
     output logic error,
 
-    output logic [7:0] display,
-    output logic [2:0] state_out
+    output logic [2:0] state_out,
 
     // SINAIS INTERMÓDULOS
 
-    input logic change,             // Output do subtrator
     input logic can_sell,           // Output do comparador
-
-    input logic [7:0] credit,       // Crédito guardado em credit_reg
 
     output logic credit_load,       // Ativa registrador de crédito
     output logic credit_op,         // Operação a ser realizada pelo registrador de crédito 
 
     output logic mem_read,          // Ativa leitura da memória - price e stock
-    output logic mem_write,         // Ativa escrita da memória - decrementa stock
+    output logic mem_write          // Ativa escrita da memória - decrementa stock
 
 );
 
@@ -33,42 +27,45 @@ module control_unit (
 
     state_t state;
 
-    logic [7:0] troco;
-
-
     always_ff @(posedge clk) begin
         if (rst || cancel) begin
-            state <= IDLE;        
-            troco <= 0;
-        
+            state <= IDLE;
         end else begin
             case (state)
                 IDLE: begin
                     if(coin_in != 2'b00) begin
                         state <= COLLECT; 
                     end
-                    else if (confirm) begin
-                        state <= CHECK;
-                    end
                 end
 
                 COLLECT: begin
-                    if(coin_in == 2'b00) begin
-                        state <= IDLE;
+                    if (confirm) begin
+                        state <= CHECK_ACTIVATE_MEMORY;
                     end
                 end
 
+                CHECK_ACTIVATE_MEMORY: begin
+                    state <= CHECK;
+                end
+
                 CHECK: begin
-                    if(can_sell) state <= DISPENSE;
-                    else state <= ERROR;
+                    if (can_sell) begin
+                        state <= DISPENSE;
+                    end else begin
+                        state <= ERROR;
+                    end
                 end
 
                 DISPENSE: begin
+                    state <= CHANGE_ACTIVATE_MEMORY;
+                end
+
+                CHANGE_ACTIVATE_MEMORY: begin
                     state <= CHANGE;
                 end
 
                 CHANGE: begin
-                    troco <= subtracao;
+                    state <= IDLE;
                 end
 
                 ERROR: begin
@@ -91,7 +88,6 @@ module control_unit (
         mem_read = 0;
         dispense = 0;
         mem_write = 0;
-        change_out = 0;
         error = 0;
 
         case (state)
@@ -103,6 +99,10 @@ module control_unit (
                 credit_op = 0;
             end
 
+            CHECK_ACTIVATE_MEMORY: begin
+                mem_read = 1;
+            end
+
             CHECK: begin
                 mem_read = 1;
             end
@@ -112,8 +112,11 @@ module control_unit (
                 mem_write = 1;
             end
 
+            CHANGE_ACTIVATE_MEMORY: begin
+                mem_read = 1;
+            end
+
             CHANGE: begin
-                change_out = credit - price; // TODO implementar leitura de price
                 credit_load = 1;
                 credit_op = 1;
             end
@@ -129,7 +132,6 @@ module control_unit (
                 mem_read = 0;
                 dispense = 0;
                 mem_write = 0;
-                change_out = 0;
                 error = 0;
             end
 
